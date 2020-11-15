2313248925afc44585eb4ec4d86c8b635ad3a634
Isaias Neto <isaiasneto@gmail.com>
2020-11-15 19:31:25 -0300

    Removed unecessary lines

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -10,7 +8,6 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
