fda212e8d222c05f510207292a70cd86c9c595ed
Isaias Neto <isaiasneto@gmail.com>
2020-11-21 14:11:46 -0300

    Renamed const and removed scope

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const indexRouter = require('./routes/index');
+const routes = require('./routes');
 
 const app = express();
 
@@ -10,6 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', indexRouter);
+app.use(routes);
 
 module.exports = app;
