7a7908e7debcb505b9b73021088a79f1cb224ec0
Isaias Neto <isaiasneto@gmail.com>
2020-11-21 14:12:31 -0300

    Created two new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const EmployeeControllerIndex = require('../controllers/employeeController');
+const OrderLogController = require('../controllers/orderLogController');
 
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.json({ status: 'OK' });
+});
+
+router.get('/employee', EmployeeControllerIndex);
+
+router.get('/orderLog', (req, res, next) => {
+  const { startDate, endDate } = req.query;
+
+  if (startDate && endDate) {
+    const filters = {
+      startDate,
+      endDate
+    };
+    return OrderLogController.dateRange(req, res, filters);
+  }
+
+  OrderLogController.index(req, res);
 });
 
 module.exports = router;
