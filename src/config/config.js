371e53255bcb356f1d362c908cd85b3c03885cbc
Isaias Neto <isaiasneto@gmail.com>
2020-11-21 18:45:02 -0300

    Created migrate config files

---
+++ src/config/config.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const config = {
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT
+  },
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": `${process.env.DB_NAME}_test`,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT
+  }
+};
+
+module.exports = config;
